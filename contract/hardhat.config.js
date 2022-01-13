96d7af0cfbef3861844093a730095eaa181ed35b
Chidi <profchydon@gmail.com>
2022-01-13 21:45:34 +0100

    Restore files

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,11 +1,15 @@
 require('@nomiclabs/hardhat-waffle');
 
+require('dotenv').config();
+
+const key = process.env.key;
+
 module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/PuJorBOym2hflvKGbdeujyrkr_mqwZsG',
-      accounts: ['9dea9b9ee124125af6b079117514b97201d09b17fa2e373cf1b6cb58470aed59']
+      accounts: [key]
     }
   }
 }
